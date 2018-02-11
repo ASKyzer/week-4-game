9418b7947adf7fa555467a96dc078e5ce50e10c9
Adrian Kyzer <ASKyzer@github.com>
2018-02-11 18:11:12 -0500

    generated random numbers for target score and each characters.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,14 +19,43 @@ var keroppiValue;
 
 // when the game is initialized
 function startGame(){
+  //i want the total value = 0
 
+}
   //random number from 19-120 generated
+  function randomNum(min,max){
 
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
+  randomNum(19,120);
 
-  //random values are assigned to characters between 1-12
+  //random values are assigned to characters between 1-12//
+  function randomBatzValue(min,max){
+    // random batz value
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
+  randomBatzValue(1,12);
+
+  function randomKittyValue(min,max){
+    // random kitty value
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
+  randomKittyValue(1,12);
+
+  function randomPenguinValue(min,max){
+    // random penguin value
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
+  randomPenguinValue(1,12);
+
+  function randomKeroppiValue(min,max){
+    // random keroppivalue
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
+  randomKeroppiValue(1,12);
 
   //your sum is set to zero
-}
+
 
 // playing the game
 
@@ -45,9 +74,18 @@ function startGame(){
 // if totalValue === randomNum, win++
 
 // when you win or lose, the game resets, but the wins and losses don't clear
-
-
-
+function reset(){
+  totalValue = 0;
+  randomNum();
+}
+////////////////////
+// FOR DEBUGGING //
+//////////////////
+console.log(randomNum(19,120));
+console.log(randomBatzValue(1,12));
+console.log(randomKittyValue(1,12));
+console.log(randomPenguinValue(1,12));
+console.log(randomKeroppiValue(1,12));
 
 
 
