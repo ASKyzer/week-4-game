f6f225127f9b8b374703a5ed40705e3fe1b4b9fe
Adrian Kyzer <ASKyzer@github.com>
2018-02-11 17:37:09 -0500

    pseudocoded the game and added some variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,50 @@
 $(document).ready(function(){
 
+//////////////////////
+// Global Variable //
+////////////////////
 
+var wins = 0;
+var losses = 0;
+var totalValue = 0;
+var randomNum;
+var batzValue;
+var kittyValue;
+var penguinValue;
+var keroppiValue;
+
+////////////////
+// Functions //
+//////////////
+
+// when the game is initialized
+function startGame(){
+
+  //random number from 19-120 generated
+
+
+  //random values are assigned to characters between 1-12
+
+  //your sum is set to zero
+}
+
+// playing the game
+
+// click on a charactor to get random values
+
+// that value is then printed on the screen
+
+// click on another character to get another random values
+
+// that value is then added to the first values
+
+// if totalValue is less than randomNum, the game continues
+
+// if totalValue is greater than randomNum, the game ends and losses++
+
+// if totalValue === randomNum, win++
+
+// when you win or lose, the game resets, but the wins and losses don't clear
 
 
 
