436481704f5bb8634489cb5ce7be150e2f6aac38
Adrian Kyzer <ASKyzer@github.com>
2018-02-12 00:29:08 -0500

    change all ids and classes to match pokemon theme

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
 
 var wins = 0;
 var losses = 0;
-var totalValue = 0;
+// randomly generated number of pokemons to collect
 var numToReach;
 // values of each character
-var batzValue;
+var batzValues;
 var kittyValue;
 var penguinValue;
 var keroppiValue;
@@ -27,8 +27,8 @@ var runningTotal = 0;
   //random number from 19-120 generated
   numToReach = Math.floor(Math.random()*(maxCompNum-minCompNum+1)+minCompNum);
   // that value is then printed on the screen
-  $('#ranNum').html(numToReach);
-
+  $('#monstersToCollect').html(numToReach);
+  // randomly generated number of each character
   batzValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
   kittyValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
   penguinValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
@@ -41,13 +41,12 @@ var runningTotal = 0;
 // to set all variables back to zero
   function restart(){
 
-    $('#yourTotal').html("0");
+    $('#totalMonsters').html("0");
 
-    totalValue = 0;
     runningTotal = 0;
     numToReach = Math.floor(Math.random()*(maxCompNum-minCompNum+1)+minCompNum);
       // that value is then printed on the screen
-    $('#ranNum').html(numToReach);
+    $('#monstersToCollect').html(numToReach);
 
     batzValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
     kittyValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
@@ -87,30 +86,30 @@ function compareScore() {
 /////////////////////
 
   // click on a charactor to get random values //
-  $("#batz").on("click", function(){
+  $("#dragon").on("click", function(){
     // adds the value of the character to the new total
     runningTotal = runningTotal + batzValue;
     // outputs to DOM
-    $('#yourTotal').html(runningTotal);
+    $('#totalMonsters').html(runningTotal);
     // calls on the function to evaluate win or loss
     compareScore();
   })
   // the events below are the same as described above
-  $("#kitty").on("click", function(){
+  $("#wartortle").on("click", function(){
     runningTotal = runningTotal + kittyValue;
-    $('#yourTotal').html(runningTotal);
+    $('#totalMonsters').html(runningTotal);
     compareScore();
   })
 
-  $("#penguin").on("click", function(){
+  $("#banette").on("click", function(){
     runningTotal = runningTotal + penguinValue;
-    $('#yourTotal').html(runningTotal);
+    $('#totalMonsters').html(runningTotal);
     compareScore();
   })
 
-  $("#keroppi").on("click", function(){
+  $("#kobra").on("click", function(){
     runningTotal = runningTotal + keroppiValue;
-    $('#yourTotal').html(runningTotal);
+    $('#totalMonsters').html(runningTotal);
     compareScore();
   })
 
