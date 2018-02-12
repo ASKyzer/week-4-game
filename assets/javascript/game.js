b1ba3d10eb27607049cda81350657ffea082f374
Adrian Kyzer <ASKyzer@github.com>
2018-02-11 21:19:22 -0500

    creat funtion to compare score and call to check on click.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,93 +21,77 @@ var minCompNum = 19;
 var maxCompNum = 120;
 var runningTotal = 0;
 
-////////////////
-// Functions //
-//////////////
+// When the game starts, all the values are randomly generated but only the score to match is shown
 
-// when the game is initialized
-function startGame(){
-  //i want the total value = 0
-
-}
   //random number from 19-120 generated
-
   numToReach = Math.floor(Math.random()*(maxCompNum-minCompNum+1)+minCompNum);
-    // that value is then printed on the screen
+  // that value is then printed on the screen
   $('#ranNum').html(numToReach);
 
   batzValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
-
   kittyValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
-
   penguinValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
-
   keroppiValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
 
-// to set everything back to zero
+// to set all variables back to zero
   function restart(){
+
     totalValue = 0;
     runningTotal = 0;
-    numToReach = 0;
+    numToReach = Math.floor(Math.random()*(maxCompNum-minCompNum+1)+minCompNum);
+      // that value is then printed on the screen
+    $('#ranNum').html(numToReach);
+
+    batzValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
+    kittyValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
+    penguinValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
+    keroppiValue = Math.floor(Math.random()*(maxCharNum-minCharNum+1)+minCharNum);
+  };
+
+// Comparing the score to see who Wins
+function compareScore() {
+  if (runningTotal == numToReach) {
+    alert("Congratulations!  You've won this round.");
+    win++;
+    $('#wins').html(wins);
+    restart();
   }
+    else if (runningTotal > numToReach){
+      alert("Sorry!  Better luck next time.");
+      losses++;
+      $('#losses').html(losses);
+      restart();
+    }
+};
+
 // playing the game
 
-// click on a charactor to get random values
+// click on a charactor to get random values //
 $("#batz").on("click", function(){
   runningTotal = runningTotal+batzValue;
-  $('#yourTotal').html(runningTotal);
+  $('#yourTotal').html(runningTotal);  // outputs to DOM
+  compareScore();
 })
 
 $("#kitty").on("click", function(){
   runningTotal = runningTotal+kittyValue;
   $('#yourTotal').html(runningTotal);
+  compareScore();
 })
 
 $("#penguin").on("click", function(){
   runningTotal = runningTotal+penguinValue;
   $('#yourTotal').html(runningTotal);
+  compareScore();
 })
 
 $("#keroppi").on("click", function(){
   runningTotal = runningTotal+keroppiValue;
   $('#yourTotal').html(runningTotal);
+  compareScore();
 })
 
-function scoreComparison() {
-  if ( runningTotal === numToReach) {
-    win++;
-    $('#wins').html(wins);
-    restart();
-  }
-}
 
-
-
-
-// click on another character to get another random values
-
-// that value is then added to the first values
-
-// if totalValue is less than randomNum, the game continues
-
-// if totalValue is greater than randomNum, the game ends and losses++
-function evaluateScores() {
-    if (totalValue > numToReach) {
-      losses++;
-      alert("Better luck next time.")
-      restart();
-    }
-  }
-
-// if totalValue === randomNum, win++
-    if (totalValue === numToReach) {
-      wins++;
-      alert("Congratulations!  You've won.");
-      restart();
-    }
-    // when total value exceeds the random number
-
-// when you win or lose, the game resets, but the wins and losses don't clear
 
 
 ////////////////////
